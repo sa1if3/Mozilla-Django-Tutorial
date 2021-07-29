ed2c27138f3baf808027bd93e25c43d3f36a6fc3
sa1if3 <saifur.rahman18@gmail.com>
2021-07-29 15:39:43 +0000

    Update settings.py

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -28,7 +28,7 @@ SECURE_HSTS_PRELOAD = os.environ.get('DJANGO_SECURE_HSTS_PRELOAD', '') != 'False
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','xs4e(gblixg6ryz6@3xo526mrwdaf^dpbs1c9erujo0_&42(uc')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
